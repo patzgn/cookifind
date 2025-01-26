3de94f4d290bf2e49f2120971f32f8c7aaf244b1
patzgn <patrycja.zgierun@gmail.com>
2025-01-26 10:06:47 +0100

    Add Scalar

--- CookiFind/CookiFind.Api/Program.cs
+++ CookiFind/CookiFind.Api/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +11,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
